0d751348231657fb307728e2acdb12036b960f4d
Sam <sael12@hotmail.es>
2025-08-11 15:44:06 +0200

    prod api key stripe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://book-management-dev.onrender.com/v1',
-  stripePublicKey: 'pk_test_51ReswyIsUbiUMsbMgVn2lygzeh2Emgct9ReqonVfqNNJFPm8o5TWVpE9lDhhEEcE5GboKHEx9N49MhlkXECaGGoy00VxmikJNY',
+  stripePublicKey: 'pk_live_51ReswpEyRl3HoSeoDtfZZrpG4oujAuNoVOHyge6jSC82N5eLczaf9jfia7N5l3RdmDkfKOLsm2zVhMj5ZpP1CHAV00NFt38nGn',
   price: 1
 };
