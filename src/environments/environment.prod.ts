d7164a235d9a9ec657a906a685edfacba34d6fb2
Sam <sael12@hotmail.es>
2025-08-11 15:57:11 +0200

    modigy price

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://book-management-prod.onrender.com/v1',
   stripePublicKey: 'pk_live_51ReswpEyRl3HoSeoDtfZZrpG4oujAuNoVOHyge6jSC82N5eLczaf9jfia7N5l3RdmDkfKOLsm2zVhMj5ZpP1CHAV00NFt38nGn',
-  price: 1
+  price: 8
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://book-management-dev.onrender.com/v1',
   stripePublicKey: 'pk_live_51ReswpEyRl3HoSeoDtfZZrpG4oujAuNoVOHyge6jSC82N5eLczaf9jfia7N5l3RdmDkfKOLsm2zVhMj5ZpP1CHAV00NFt38nGn',
-  price: 1
+  price: 8
 };
