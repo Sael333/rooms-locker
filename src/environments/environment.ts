a9b2bd6d1b219d303bc5a5fa441d6872f315fb11
Sam <sael12@hotmail.es>
2025-08-26 15:59:29 +0200

    change route

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://book-management-prod.onrender.com/v1',
+  apiUrl: 'https://book-management-dev.onrender.com',
   stripePublicKey: 'pk_test_51ReswyIsUbiUMsbMgVn2lygzeh2Emgct9ReqonVfqNNJFPm8o5TWVpE9lDhhEEcE5GboKHEx9N49MhlkXECaGGoy00VxmikJNY',
   price: 8,
   plusPrice: 2
