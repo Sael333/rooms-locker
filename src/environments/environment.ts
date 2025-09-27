6ae0fa3afd9e45b13b7ceb0d552ff96c854b57ee
Sam <sael12@hotmail.es>
2025-09-27 11:41:18 +0200

    build for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://book-management-dev.onrender.com/v1',
   stripePublicKey: 'pk_live_51ReswpEyRl3HoSeoDtfZZrpG4oujAuNoVOHyge6jSC82N5eLczaf9jfia7N5l3RdmDkfKOLsm2zVhMj5ZpP1CHAV00NFt38nGn',
-  price: 8,
+  price: 6,
   plusPrice: 2
 };
